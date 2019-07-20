ddfc7bcbb9246ad050375ade891d703a365ee398
Robin Millette <robin@millette.info>
2019-07-20 17:36:30 -0400

    Use vega directly (no vega-lite or vega-embed).

--- js/main.js
+++ js/main.js
@@ -1,13 +1,21 @@
-// npm
-import embed from "vega-embed"
-
 // self
-import spec from "./spec.json"
+import spec from "./s2.json"
 import data from "../f2.json"
 
-embed("#viz", spec)
-  .then(({ view }) => Promise.all([
-    view.insert("funding", data),
-    view.runAsync(),
-  ]))
+// npm
+import * as vega from "vega"
+
+const n1 = Date.now()
+const view = new vega.View(
+  vega.parse(spec),
+  { container: "#viz", hover: true }
+)
+
+const n2 = Date.now()
+console.log('T1', n2 - n1)
+
+view
+  .insert("funding", data)
+  .runAsync()
+  .then(() => console.log('T2', Date.now() - n2))
   .catch(console.error)
