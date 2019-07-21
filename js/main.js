7da12a46a75d9609d361b19a47209e4c7ec5d21b
Robin Millette <robin@millette.info>
2019-07-20 21:45:23 -0400

    Use simpler vega interface.

--- js/main.js
+++ js/main.js
@@ -1,21 +1,10 @@
 // self
-import spec from "./s2.json"
-import data from "../f2.json"
+import specA from "./s2.json"
+import dataA from "../f2.json"
 
-// npm
-import * as vega from "vega"
+// self
+// import show from "./show.js"
+import show from ".."
 
-const n1 = Date.now()
-const view = new vega.View(
-  vega.parse(spec),
-  { container: "#viz", hover: true }
-)
-
-const n2 = Date.now()
-console.log('T1', n2 - n1)
-
-view
-  .insert("funding", data)
-  .runAsync()
-  .then(() => console.log('T2', Date.now() - n2))
-  .catch(console.error)
+show(specA, { container: "#viz", dataName: "funding", data: dataA })
+  .catch((e) => console.error('Oupsy', e))
